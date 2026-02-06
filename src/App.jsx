b600d4bb926a83db1f69060fb1e478c74eaef0a4
omkar hole <omkarhole314@gmail.com>
2026-02-06 16:28:09 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import SizeGuide from './pages/SizeGuide'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/size-guide" element={<SizeGuide />} />
         {/* Add other routes here as we migrate them */}
       </Routes>
     </Router>
